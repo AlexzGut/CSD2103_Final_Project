26ac9e12f13969c5c642d63f89b81980da9d0073
Alexander Gutierrez <alexzgut.dev@gmail.com>
2023-11-11 12:27:25 -0500

    initial comit

---
+++ php/signup.php
@@ -0,0 +1,35 @@
+<?php
+
+$servername = "localhost";
+$username = "web";
+$password = "web";
+$dbname = "registration";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+  }
+echo "Connected successfully";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $firstname = mysqli_real_escape_string($conn, $_POST['first-name']);
+    $lastname = mysqli_real_escape_string($conn, $_POST['last-name']);
+    $email = mysqli_real_escape_string($conn, $_POST['email']);
+    $username = mysqli_real_escape_string($conn, $_POST['username']);
+    $password = mysqli_real_escape_string($conn, $_POST['password']);
+    $hashed_password = password_hash($password, PASSWORD_BCRYPT);
+
+    $sqlstatement = "INSERT INTO users (first_name, last_name, email, username, password) VALUES ('$firstname', '$lastname', '$email', '$username', '$hashed_password')";
+    mysqli_query($conn, $sqlstatement);
+}
+// $result = mysqli_query($conn, $sqlstatement);
+// if ($result) {
+//   echo "Data inserted successfully!";
+// }
+
+header('location:../html/signup.html');
+mysqli_close($conn);
+?>
