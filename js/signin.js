823a86728bac20a25b0c000c0e0792234bd4bb30
Alexander Gutierrez <alexzgut.dev@gmail.com>
2023-11-24 11:07:39 -0500

    adds jQuery validation to signin and signup pages

--- js/signin.js
+++ js/signin.js
@@ -1,7 +1,32 @@
-const signin = function() {
+const signin = function () {
     $('#signin-form').submit();
 };
 
-$(function() {
+$(function () {
     $('#btn-submit').on('click', signin);
+
+
+    // Validation Using jQuery Validation
+    $('#signin-form').validate({
+        rules: {
+            username: {
+                required: true,
+
+                normalizer: function (value) {
+                    return $.trim(value);
+                }
+            },
+            password: {
+                required: true
+            }
+        },
+        messages: {
+            username: {
+                required: "Please enter the username"
+            },
+            password: {
+                required: "Please enter the password"
+            }
+        }
+    });
 });
--- js/signup.js
+++ js/signup.js
@@ -11,4 +11,32 @@ $(function() {
         yearRange: '-100:+0'});
     
     $('#btn-submit').on('click', signup);
+
+    // jQuery validation
+    $('#signup-form').validate({
+        rules: {
+            'first-name': {
+                required: true
+            },
+            'last-name': {
+                required: true
+            },
+            email: {
+                required: true
+            },
+            phone: {
+                required: true
+            },
+            'date-of-birth': {
+                required: true
+            },
+            username: {
+                required: true
+            },
+            'confirm-password': {
+                required: true,
+                equalTo: '#password'
+            }
+        }
+    });
 });
