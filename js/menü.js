0e5631fbe658554532a8a3d2ed6e5b57231cd179
ZorbeyTarhan <80391487+ZorbeyTarhan@users.noreply.github.com>
2023-11-24 09:29:42 -0500

    Update menu.js

--- js/menü.js
+++ js/menü.js
@@ -21,18 +21,45 @@ document.addEventListener("DOMContentLoaded", function () {
                 <img src="../img/${item.image}" alt="${item.name}">
                 <h2>${item.name}</h2>
                 <p>${item.description}</p>
+                <p>Price: $${item.price.toFixed(2)}</p>
+                <div class="cart-controls">
+                    <button class="decrement">-</button>
+                    <span class="item-count">0</span>
+                    <button class="increment">+</button>
+                </div>
                 <button class="addToCartButton">Add to Cart</button>
             `;
             menuContainer.appendChild(section);
-        }
 
-        const addToCartButtons = document.querySelectorAll('.addToCartButton');
+            const addToCartButton = section.querySelector('.addToCartButton');
+            const incrementButton = section.querySelector('.increment');
+            const decrementButton = section.querySelector('.decrement');
+            const itemCountSpan = section.querySelector('.item-count');
 
-        addToCartButtons.forEach(button => {
-            button.addEventListener('click', function () {
-                alert('The item is added to cart');
+            addToCartButton.addEventListener('click', function () {
+                const itemCount = parseInt(itemCountSpan.textContent);
+                if (itemCount > 0) {
+                    alert(`Added ${itemCount} ${item.name}(s) to the cart`);
+                    itemCountSpan.textContent = '0'; // Sayacı sıfırla
+                } else {
+                    alert('Please select the quantity first');
+                }
             });
-        });
+
+            incrementButton.addEventListener('click', function () {
+                let itemCount = parseInt(itemCountSpan.textContent);
+                itemCount++;
+                itemCountSpan.textContent = itemCount;
+            });
+
+            decrementButton.addEventListener('click', function () {
+                let itemCount = parseInt(itemCountSpan.textContent);
+                if (itemCount > 0) {
+                    itemCount--;
+                    itemCountSpan.textContent = itemCount;
+                }
+            });
+        }
     }
 
     function updatePaginationButtons() {
@@ -64,15 +91,15 @@ document.addEventListener("DOMContentLoaded", function () {
     homeButton.addEventListener("click", goHome);
 
     const sampleMenuData = [
-        { name: "Dish 1", description: "Description 1", image: "image1.jpg" },
-        { name: "Dish 2", description: "Description 2", image: "image2.jpg" },
-        { name: "Dish 3", description: "Description 3", image: "image3.jpg" },
-        { name: "Dish 4", description: "Description 4", image: "image4.jpg" },
-        { name: "Dish 5", description: "Description 5", image: "image5.jpg" },
-        { name: "Dish 6", description: "Description 6", image: "image6.jpg" },
-        { name: "Dish 7", description: "Description 7", image: "image7.jpg" },
-        { name: "Dish 8", description: "Description 8", image: "image8.jpg" },
-        { name: "Dish 9", description: "Description 9", image: "image9.jpg" },
+        { name: "Dill-i-cio", description: "Panko battered and fried pickle, fiery Sriracha, savoury bacon, garlic aioli, lettuce, and tomato.", image: "images1.jpg", price: 10.99 },
+        { name: "Cheese Burger", description: "Red onion , pickles, lettuce, tomatoes with our tasty garlic aioli.", image: "images2.jpg", price: 8.99 },
+        { name: "Big Cheese Burger", description: "With 12 oz beef patty, red onion, pickles, lettuce, tomato, garlic aioli and double cheddar cheese", image: "images3.jpg", price: 16.99 },
+        { name: "Lamby Burger", description: "Grilled red peppers and goat cheese with smoky chipotle aioli, bacon, arugula and tomatoes.", image: "images4.jpg", price: 11.99  },
+        { name: "The Grilled Cheese Bacon Burger", description: "Cheddar, mozzarella, lettuce, tomato and savoury bacon, with our smoky chipotle aioli.", image: "images5.jpg", price: 14.99  },
+        { name: "Jerk Chicken Burger", description: "Grilled chicken breast, jerk sauce, jalapenos, red onion, lettuce, and tomato.", image: "images6.jpg", price: 13.99  },
+        { name: "Red Melt Burger", description: "Roasted red peppers, mozarella cheese, chipotle aioli, lettuce, tomato", image: "images7.jpg", price: 13.99  },
+        { name: "Riverside Burger", description: "Onion ring, mozzarella, bacon, bbq sauce, garlic aioli, lettuce, and tomato.", image: "images8.jpg", price: 14.99  },
+        { name: "The Big Boy Burger", description: "Double beef patty (11 oz) with double cheddar cheese, red onion, pickles, fried egg, jalapeno, garlic ailoli, lettuce, and tomato.", image: "images9.jpg", price: 18.50  },
     ];
 
     menuData.push(...sampleMenuData);
