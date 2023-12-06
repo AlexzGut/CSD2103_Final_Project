8c4d00b1cc76d11b8e33ef61f495f50147ebff53
Paul Jordan <pauljordanuntalan@gmail.com>
2023-12-05 19:33:51 -0500

    updated js and html to show the name, phone and email on myaccount webpage

--- js/myaccount.js
+++ js/myaccount.js
@@ -1,25 +1,55 @@
+document.addEventListener('DOMContentLoaded', function() {
+  const userinfoString = localStorage.getItem('userInfo');
+  const userInfo = JSON.parse(userinfoString);
 
-$(document).ready(function() {
 
-    $( "#accordion-edit-account" ).accordion({
-      active: 4,
-      collapsible: true,
-      heightStyle: "content"
-    });
+  const firstName = userInfo.firstname;
+  const lastName = userInfo.lastname;
+  const phone = userInfo.phone;
+  const email = userInfo.email;
+  // const address = userInfo.address;
 
-    $( "#accordion-help-faq" ).accordion({
-      active: 0,
-      collapsible: true,
-      heightStyle: "content"
-    });
+  const nameContainer = document.getElementById('user-name-placeholder');
+  if (nameContainer) {
+    nameContainer.textContent = `${firstName} ${lastName}`;
+  }
+
+  const phoneContainer = document.getElementById('user-phone-placeholder');
+  if (phoneContainer) {    
+    const lastFourDigits = phone.slice(-4);
+    phoneContainer.textContent = `***-***-${lastFourDigits}`;
+  }
+
+  const emailContainer = document.getElementById('user-email-placeholder');
+  if (emailContainer) {
+    emailContainer.textContent = `${email}`;
+  }
+
+  // const addressContainer = document.getElementById('user-address-placeholder');
+  // if (addressContainer) {
+  //   emailContainer.textContent = `${address}`;
+  // }
+
+  $(document).ready(function() {
+
+      $( "#accordion-edit-account" ).accordion({
+        active: 4,
+        collapsible: true,
+        heightStyle: "content"
+      });
+
+      $( "#accordion-help-faq" ).accordion({
+        active: 0,
+        collapsible: true,
+        heightStyle: "content"
+      });
+      
+      const logout = function () {
+        window.location.href = '../php/logout.php';
+      };
+
+      $('.button-logout, .logoutUser').on('click', logout);
     
-    const logout = function () {
-      window.location.href = '../php/logout.php';
-    };
+  });
 
-    $('.button-logout, .logoutUser').on('click', logout);
-  
 });
-
-
-
